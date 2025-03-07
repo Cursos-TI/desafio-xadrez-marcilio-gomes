cf2c011485cfcba7042ad15c91c18ae7a51d3363
Marcilio Gomes <123701492+marcilio-gomes@users.noreply.github.com>
2025-03-07 19:45:45 +0000

    Finalizado

--- Mestre.c
+++ Mestre.c
@@ -3,7 +3,7 @@
 
 // Desafio de Xadrez - MateCheck
  
-// Função da movimento da Torre
+// Função do movimento da Torre
 void movimentoTorre(int n) {
     if (n > 0) {  // Verifica se ainda há passos para a recursão
         for (int t = 0; t <= 5; t++) {
@@ -14,6 +14,7 @@ void movimentoTorre(int n) {
     }
 }
 
+// Função do movimento do Bispo
 void movimentoBispo(int n) {
     if (n > 0) {
         for (int i = 1; i <= 5; i++) {
@@ -30,6 +31,7 @@ void movimentoBispo(int n) {
     }
 }
 
+// Função do movimento do Rainha
 void movimentoRainha(int n) {
     if (n > 0) {
         for (int i = 1; i <= 8; i++) {
@@ -40,6 +42,7 @@ void movimentoRainha(int n) {
     }
 }
 
+// Função do movimento do Cavalo
 void movimentoCavalo(int n) {
     if (n > 0) {
 
@@ -58,22 +61,30 @@ void movimentoCavalo(int n) {
 
 
 
-
+// Função Principal onde vai ser execultado as funções de cada peça
 int main() {
 
+    // Variaveis
     int torre = 1, bispo = 1, rainha = 1, cavalo = 1;
     printf("*** MateCheck ***\n");
 
     printf("\nMovimento Torre:\n\n");
+    //executando a função da torre
     movimentoTorre(torre);
 
+    
     printf("\nMovimento Bispo\n\n");
+    //executando a função do bispo
     movimentoBispo(bispo);
 
     printf("\nMovimento Rainha\n\n");
+    //executando a função da rainha
     movimentoRainha(rainha);
 
     printf("\nMovimento Cavalo\n\n");
+    //executando a função do cavalo
     movimentoCavalo(cavalo);
-    return 0;
+
+
+    return 0; // programa executado com sucesso
 }
